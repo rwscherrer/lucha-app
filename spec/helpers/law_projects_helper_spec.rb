6d32ae94fdfebb239acdb86f787946bd2a996ad6
heydanhey <danhey@gmail.com>
2017-01-10 20:53:51 -0600

    remove helper specs

--- spec/helpers/law_projects_helper_spec.rb
+++ spec/helpers/law_projects_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe LawProjectsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe LawProjectsHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/helpers/rentals_helper_spec.rb
+++ spec/helpers/rentals_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe RentalsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe RentalsHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
